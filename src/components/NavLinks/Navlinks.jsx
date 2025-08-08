b29049c6853047f0f866d1755ab1b750fd9bf122
deimos19 <devadarshm2001@gmail.com>
2025-08-08 19:33:40 +0530

    added navlinks

--- src/components/NavLinks/Navlinks.jsx
+++ src/components/NavLinks/Navlinks.jsx
@@ -1,30 +1,44 @@
 import React from 'react';
 
 const Navlinks = () => {
-  const links = [
-    'Cinema',
-    'Can Politics',
-    'Can Exclusive',
-    'Popular News',
-    'Cele Videos',
-    'Popular Videos',
-    'Astro',
-    'New Showcases',
-    'Trailer/Teaser/Songs'
-  ];
-
   return (
-    <div className="w-full flex justify-center items-center p-4 overflow-x-auto">
-      <div className="flex gap-4 flex-wrap justify-between">
-        {links.map((link, index) => (
-          <a
-            key={index}
-            href="#"
-            className="text-sm font-medium hover:underline whitespace-nowrap"
-          >
-            {link}
-          </a>
-        ))}
+    <div className="w-full">
+      {/* For large screens */}
+      <div className="hidden md:flex justify-center items-center p-4 overflow-x-auto">
+        <ul className="flex gap-4 flex-wrap justify-between">
+          <li><a href="#" className="text-sm font-medium hover:underline whitespace-nowrap">Cinema</a></li>
+          <li><a href="#" className="text-sm font-medium hover:underline whitespace-nowrap">Can Politics</a></li>
+          <li><a href="#" className="text-sm font-medium hover:underline whitespace-nowrap">Can Exclusive</a></li>
+          <li><a href="#" className="text-sm font-medium hover:underline whitespace-nowrap">Popular News</a></li>
+          <li><a href="#" className="text-sm font-medium hover:underline whitespace-nowrap">Cele Videos</a></li>
+          <li><a href="#" className="text-sm font-medium hover:underline whitespace-nowrap">Popular Videos</a></li>
+          <li><a href="#" className="text-sm font-medium hover:underline whitespace-nowrap">Astro</a></li>
+          <li><a href="#" className="text-sm font-medium hover:underline whitespace-nowrap">New Showcases</a></li>
+          <li><a href="#" className="text-sm font-medium hover:underline whitespace-nowrap">Trailer/Teaser/Songs</a></li>
+        </ul>
+      </div>
+
+      {/* For small screens */}
+      <div className="md:hidden p-4">
+        <div className="collapse collapse-arrow bg-base-200">
+          <input type="checkbox" />
+          <div className="collapse-title text-md font-semibold">
+            Show Content
+          </div>
+          <div className="collapse-content">
+            <ul className="flex flex-col gap-2">
+              <li><a href="#" className="text-sm font-medium hover:underline">Cinema</a></li>
+              <li><a href="#" className="text-sm font-medium hover:underline">Can Politics</a></li>
+              <li><a href="#" className="text-sm font-medium hover:underline">Can Exclusive</a></li>
+              <li><a href="#" className="text-sm font-medium hover:underline">Popular News</a></li>
+              <li><a href="#" className="text-sm font-medium hover:underline">Cele Videos</a></li>
+              <li><a href="#" className="text-sm font-medium hover:underline">Popular Videos</a></li>
+              <li><a href="#" className="text-sm font-medium hover:underline">Astro</a></li>
+              <li><a href="#" className="text-sm font-medium hover:underline">New Showcases</a></li>
+              <li><a href="#" className="text-sm font-medium hover:underline">Trailer/Teaser/Songs</a></li>
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
   );
